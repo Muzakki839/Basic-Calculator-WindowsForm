1043c13f8b968e3ca81d91e40b1ef07c8f9f4f04
muhammad aziz <azizkeren2004@gmail.com>
2024-01-04 13:17:32 +0700

    login

---
+++ Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicCalculator
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "admin" && txtUser.Text == "admin")
+            {
+                this.Hide();
+                Form1 ss = new Form1();
+                ss.Show();
+            }
+            else
+                MessageBox.Show("username or password is incorrect");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
